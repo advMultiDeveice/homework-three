800b72f77bb2b827790d38523ab9c6edbdb57815
jroper7 <josephroper57@gmail.com>
2024-09-29 18:31:23 -0400

    MVC model setup

---
+++ app/app.js
@@ -0,0 +1,22 @@
+import {loadPage} from "../model/model.js";
+
+const changeRoute = () => {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace('#', '');
+
+    loadPage(pageID);
+
+}
+
+
+function initURLListener() {
+    $(window).on('hashchange', changeRoute);
+    changeRoute();
+}
+
+
+$(document).ready(function () {
+    loadPage("home")
+    initURLListener();
+    
+});
